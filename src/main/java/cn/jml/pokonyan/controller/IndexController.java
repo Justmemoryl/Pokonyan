7f0893d77e45c13e2bb2b4165678f7ad0a1f2a23
Jml <justmemoryl@foxmail.com>
2018-08-18 11:32:50 +0800

    update IndexController

--- src/main/java/cn/jml/pokonyan/controller/IndexController.java
+++ src/main/java/cn/jml/pokonyan/controller/IndexController.java
@@ -1,6 +1,7 @@
 package cn.jml.pokonyan.controller;
 
 import cn.jml.pokonyan.service.UserAccessInfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 /**
  * @version 1.0 created by Justmemoryl on 2018/7/3 16:58
  */
+@Slf4j
 @RestController
 public class IndexController {
     @Autowired private UserAccessInfoService userAccessInfoService;
